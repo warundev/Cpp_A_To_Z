814c21e6b30e456ab59b851941185cf3e4dc802f
WARUN01 <120333797+WARUNdev@users.noreply.github.com>
2023-05-13 22:06:45 +0530

    Create 06)get_the_user_input.cpp
    get the user input

---
+++ 06)get_the_user_input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+
+    cout<<"Type Your favorite number :"<<endl;
+    cin>>x;
+
+    cout<<"Your favorite number is : "<<x;
+    return 0;
+}
